914edef72a45b539d5aedd7c06d83f1dc4d74ebe
Pavan Kalyan <pavankayan14@gmail.com>
2024-06-23 21:22:14 +0530

    change config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,12 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['@monaco-editor/react']
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
     }
   }
 });
